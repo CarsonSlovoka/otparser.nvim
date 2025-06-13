894684b6e735d7b9ba50e1ae3a7c79d65a1d301c
Carson <jackparadise520a@gmail.com>
2025-06-13 15:47:57 +0800

    使用go-font來解析

--- main.go
+++ main.go
@@ -6,75 +6,51 @@
 package main
 
 import (
-	"bytes"
-	"encoding/binary"
 	"fmt"
+	"github.com/CarsonSlovoka/go-font/v1"
+	"github.com/CarsonSlovoka/go-font/v1/lib/ttlib/ttfont"
+	"github.com/CarsonSlovoka/go-font/v1/type/tag"
+	"github.com/CarsonSlovoka/otparser.nvim/internal/app"
+
 	"log"
 	"os"
-
-	"github.com/CarsonSlovoka/otparser.nvim/internal/app"
 )
 
-// OffsetTable 定義 TTF 文件的 Offset Table 結構
-type OffsetTable struct {
-	SfntVersion   uint32 // 字體版本 (0x00010000 for TrueType)
-	NumTables     uint16 // 表數量
-	SearchRange   uint16 // (最大 2 次方) * 16
-	EntrySelector uint16 // Log2(最大 2 次方)
-	RangeShift    uint16 // NumTables * 16 - SearchRange
-}
-
-// TableEntry 定義每個表的目錄項
-type TableEntry struct {
-	Tag      [4]byte // 表標籤 (如 "head", "name")
-	Checksum uint32  // 表的校驗和
-	Offset   uint32  // 表在文件中的偏移
-	Length   uint32  // 表長度
-}
-
-func main() {
+func init() {
 	log.Println("app version: ", app.Version) // log是輸出在stderr, 所以不會影響到stdout
 	if len(os.Args) < 2 {
 		fmt.Println("Usage: ttfparser <ttf_file>")
 		os.Exit(1)
 	}
+}
 
-	// 讀取 TTF 文件
-	filename := os.Args[1]
-	data, err := os.ReadFile(filename)
+func main() {
+	filepath := os.Args[1]
+
+	iFont, err := font.Dump(filepath)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Error reading file: %v\n", err)
 		os.Exit(1)
 	}
+	f := iFont.(*ttfont.File)
 
-	// 解析 Offset Table
-	var offsetTable OffsetTable
-	reader := bytes.NewReader(data)
-	if err := binary.Read(reader, binary.BigEndian, &offsetTable); err != nil {
-		fmt.Fprintf(os.Stderr, "Error parsing offset table: %v\n", err)
-		os.Exit(1)
-	}
+	_, _ = f.GetTable(tag.Head)
+	_, _ = f.GetTable(tag.Head)
+	_, _ = f.GetTable(tag.Maxp)
+	_, _ = f.GetTable(tag.OS2)
 
-	// 輸出 Offset Table 資訊
-	fmt.Printf("TTF Header Info for %s:\n", filename)
-	fmt.Printf("SFNT Version: 0x%08X\n", offsetTable.SfntVersion)
-	fmt.Printf("Number of Tables: %d\n", offsetTable.NumTables)
-	fmt.Printf("Search Range: %d\n", offsetTable.SearchRange)
-	fmt.Printf("Entry Selector: %d\n", offsetTable.EntrySelector)
-	fmt.Printf("Range Shift: %d\n", offsetTable.RangeShift)
-	fmt.Println("\nTable Directory:")
+	// 由於GetTable相依的表也會載入，所以移除不需要的表
+	f.Tables[tag.Cmap] = nil
+	f.Tables[tag.Glyf] = nil
+	f.Tables[tag.Loca] = nil
+	f.Tables[tag.Loca] = nil
+	f.Tables[tag.Hmtx] = nil
+	f.Tables[tag.Post] = nil
 
-	// 解析 Table Directory
-	for i := 0; i < int(offsetTable.NumTables); i++ {
-		var entry TableEntry
-		if err := binary.Read(reader, binary.BigEndian, &entry); err != nil {
-			fmt.Fprintf(os.Stderr, "Error parsing table entry %d: %v\n", i, err)
-			os.Exit(1)
-		}
-		fmt.Printf("Table %d:\n", i+1)
-		fmt.Printf("    Tag: %s\n", string(entry.Tag[:]))
-		fmt.Printf("    Checksum: 0x%08X\n", entry.Checksum)
-		fmt.Printf("    Offset: %d\n", entry.Offset)
-		fmt.Printf("    Length: %d\n", entry.Length)
-	}
+	// 移除其他數據
+	f.GlyphOrder = nil // 這個數據也很大，如果不想要也可以拿掉
+
+	outData, _ := f.ExportJSON()
+
+	fmt.Println(outData)
 }
