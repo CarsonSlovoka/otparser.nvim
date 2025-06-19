747d85089945a188b2ca75913f9f8c4c9d5552bd
Carson <jackparadise520a@gmail.com>
2025-06-19 17:24:33 +0800

    套用TStruct的格式來進行輸出

--- main.go
+++ main.go
@@ -11,6 +11,7 @@ import (
 	"github.com/CarsonSlovoka/go-font/v1/lib/ttlib/ttfont"
 	"github.com/CarsonSlovoka/go-font/v1/type/tag"
 	"github.com/CarsonSlovoka/otparser.nvim/internal/app"
+	"github.com/CarsonSlovoka/otparser.nvim/internal/tst"
 
 	"log"
 	"os"
@@ -19,7 +20,7 @@ import (
 func init() {
 	log.Println("app version: ", app.Version) // log是輸出在stderr, 所以不會影響到stdout
 	if len(os.Args) < 2 {
-		fmt.Println("Usage: ttfparser <ttf_file>")
+		fmt.Println("Usage: otparser <ttf_file>")
 		os.Exit(1)
 	}
 }
@@ -40,17 +41,22 @@ func main() {
 	_, _ = f.GetTable(tag.OS2)
 
 	// 由於GetTable相依的表也會載入，所以移除不需要的表
-	f.Tables[tag.Cmap] = nil
+	// f.Tables[tag.Cmap] = nil
 	f.Tables[tag.Glyf] = nil
 	f.Tables[tag.Loca] = nil
-	f.Tables[tag.Loca] = nil
 	f.Tables[tag.Hmtx] = nil
-	f.Tables[tag.Post] = nil
+	// f.Tables[tag.Post] = nil
 
 	// 移除其他數據
 	f.GlyphOrder = nil // 這個數據也很大，如果不想要也可以拿掉
 
-	outData, _ := f.ExportJSON()
+	// outData, _ := f.ExportJSON()
 
+	ts := tst.New(tst.NewConfig(" | ", "\n# ", "- "))
+	outData, err := ts.Format(f.TTFont)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error format with TStruct: %v\n", err)
+		os.Exit(1)
+	}
 	fmt.Println(outData)
 }
