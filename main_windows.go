420947c974c8d082f39526ca3c0d26b96620c3e9
Carson <jackparadise520a@gmail.com>
2025-06-23 14:31:04 +0800

    ( 移除測試用代碼 )

--- main_windows.go
+++ main_windows.go
@@ -12,24 +12,6 @@ import (
 	"github.com/CarsonSlovoka/otparser.nvim/internal/tst"
 )
 
-func outputProcess_old(f *ttfont.File) {
-	for key := range f.Tables {
-		if !slices.Contains([]tag.Tag{
-			tag.Maxp,
-		}, key) {
-			// f.Tables[key] = nil
-			continue
-		}
-		ts := tst.New(tst.NewConfig(" | ", "\n# ", "- "))
-		outData, err := ts.Format(f.Tables[key])
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "Error format with TStruct: %v\n", err)
-			os.Exit(1)
-		}
-		fmt.Println(outData) // 一次寫入在windows上，對記憶體的要求較高，如果取的表格太複雜在windows會出問題跑不出來
-	}
-}
-
 func outputProcess(f *ttfont.File) {
 	f.Tables[tag.Cmap] = nil
 	f.Tables[tag.Glyf] = nil
