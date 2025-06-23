407e47850e261281d4f8cdc40d1270573402688f
Carson <jackparadise520a@gmail.com>
2025-06-23 14:48:16 +0800

    新增TableHeader, TableRecords

--- main_windows.go
+++ main_windows.go
@@ -12,13 +12,23 @@ import (
 
 func outputProcess(f *ttfont.File) {
 	ts := tst.New(tst.NewConfig(" | ", "\n# ", "- "))
+	fmt.Printf("@TableHeader\n\n")
+	tableHeader, _ := ts.Format(f.TTFont.TableHeader)
+	fmt.Println(tableHeader)
+
+	// tableRecords, _ := ts.Format(f.TTFont.TableRecords) -- 用不了會被報錯
+	fmt.Printf("\n@TableRecords\n\n")
+	for _, tr := range f.TTFont.TableRecords {
+		tableRecords, _ := ts.Format(tr)
+		fmt.Println(tableRecords)
+	}
 
 	for tagName := range f.Tables {
 		table := f.Tables[tagName]
 		if table == nil {
 			continue
 		}
-		fmt.Printf("\n@%s\n", tagName)
+		fmt.Printf("\n@%s\n\n", tagName)
 		outData, err := ts.Format(table)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "Error format with TStruct: %v\n", err)
